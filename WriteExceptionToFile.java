542752571726f502c83290a2989abd4b7824c5ff
BaronZ <realbo89@outlook.com>
2013-07-31 14:42:57 +0800

    Update WriteExceptionToFile.java
    printError

--- WriteExceptionToFile.java
+++ WriteExceptionToFile.java
@@ -1,35 +1,23 @@
 class WriteExceptionToFile{
 
-	public void response(String className, String methodName, String json){
-		System.out.println(methodName + ":"+json);
-		String responseJson = "";
+	protected void printError(Exception e) {
+		PrintWriter pw = null;
 		try {
-			Class clz = Class.forName(className);
-			Method m1 = clz.getDeclaredMethod(methodName, String.class);
-			responseJson = (String) m1.invoke(clz.newInstance(), json);
-		} catch (Exception e) {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH");
+			sdf.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
+			String date = sdf.format(new Date());
+			String filePath = context.getRealPath("/")+"\\logs\\"+date+"-log.txt";
+			System.out.println(filePath);
+			pw = new PrintWriter(
+					new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(filePath,true),"utf-8")),true);
+			SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+			String time = sdf1.format(new Date());
+			pw.append("=================="+time+"==============================\r\n");
+			e.printStackTrace(pw);
+		} catch (Exception e1) {
 			e.printStackTrace();
-			PrintWriter pw;
-			try {
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-				String date = sdf.format(new Date());
-				String filePath = context.getRealPath("/")+"logs/"+date+"-log.txt";
-				pw = new PrintWriter(
-						new BufferedWriter(new OutputStreamWriter(
-						new FileOutputStream(filePath,true),"utf-8")),true);
-				SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-				String time = sdf1.format(new Date());
-				pw.append("=================="+time+"==============================\n");
-				e.printStackTrace(pw);
-			} catch (Exception e1) {
-				System.out.println(e1);
-			} 
-			
-			System.out.println(JsonUtils.FAIL);
-			JsonUtils.writeJSON(JsonUtils.FAIL);
 		}
-		System.out.println("response:"+responseJson);
-		JsonUtils.writeJSON(responseJson);
 	}
 
 }
